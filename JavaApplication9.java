4225125c9dfe50e0cdeb071b09a8668a7d44b156
sanduni-E <sanduniue@gmail.com>
2019-02-13 22:26:48 +0530

    positive-negetive-numbers
    positive-negetive-numbers

---
+++ JavaApplication9.java
@@ -0,0 +1,10 @@
+ public class A
+    public static void main(String[] args) {
+        int x=-5;
+        if(x>=0){
+            System.out.println("number is positive");
+        }else{
+            System.out.println("number is negative");
+        }
+    }
+}
